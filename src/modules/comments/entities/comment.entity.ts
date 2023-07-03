66edbe327e2d547fd0d258d7737081c31f9f95d8
Idan <mquinhoz@hotmail.com>
2023-07-03 14:42:40 -0300

    feat: add name contructor

--- src/modules/comments/entities/comment.entity.ts
+++ src/modules/comments/entities/comment.entity.ts
@@ -1,9 +1,12 @@
 import { randomUUID } from 'crypto';
+import { User } from 'src/modules/users/entities/User.entity';
 
 export class Comment {
   readonly id: string;
   text: string;
   user_id: string;
+  User: User;
+  name: string;
   ad_id: string;
   created_at: Date;
   updated_at: Date;
@@ -12,6 +15,7 @@ export class Comment {
   constructor() {
     this.id = randomUUID();
     this.created_at = new Date();
+    this.name = this.User.name
     //this.updated_at = null
     //this.deleted_at = null}
   }
