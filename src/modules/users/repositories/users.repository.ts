e062ddf75f662062ddf4ead10153a32dc41593bb
Idan <mquinhoz@hotmail.com>
2023-07-05 00:46:27 -0300

    fix: fix user import in repo

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,8 +1,7 @@
 import { Ad } from 'src/modules/ads/entities/ad.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
-import { User } from '../entities/User.entity';
-import { ResetTokenDto } from '../dto/reset-token.dto';
+import { User } from '../entities/user.entity';
 
 export abstract class UsersRepository {
   abstract create(data: CreateUserDto): Promise<User> | User;
